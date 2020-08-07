54657eca08b8c463ffa842a6c56e01334a60cbea
peawarrior <jacksonchen171@gmail.com>
2020-08-07 14:06:53 -0400

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((tutorial) => tutorial.split(" ").map((word) => `${word[0].toUpperCase()}${word.slice(1)}`).join(" "));
+};
