bf380e61bc304c2f53527d4288a4e7e8138effac
AlveeM <alvee.akand@outlook.com>
2020-08-08 12:14:35 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+const titleCaseSentence = sentence => {
+  return sentence.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
+}
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(titleCaseSentence);
 }
